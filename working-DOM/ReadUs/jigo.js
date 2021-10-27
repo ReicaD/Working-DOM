c2622fd924c67f34eb7b2e7474a09d2bf8978bc2
ReicaD <86073567+ReicaD@users.noreply.github.com>
2021-10-27 15:41:17 -0400

    Add files via upload

---
+++ working-DOM/ReadUs/jigo.js
@@ -0,0 +1,21 @@
+var posts = [ {
+        "title": "Bahamas",
+        "content": "great place to vacate"
+    },
+    {
+        "title": "Good Article",
+        "content": "Funeral planning"
+    },
+
+
+    {
+        "title": "Cooking",
+        "content": "It's best in the winter time"
+
+    },
+    
+    {
+        "title": "Aorli",
+        "content": "Best Salad dressing"
+    }
+]
